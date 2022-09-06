17d63ed3c3eaae633236ee8b6bf4245c152bbf64
sooyka <bartosz.sjka@gmail.com>
2022-09-06 17:46:39 +0200

    initial commit after some local work

---
+++ .bin/new_chapter.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+chapter_title=${1}
+
+mkdir "../${chapter_title}"
+touch "./TeX_files/${chapter_title}.tex"
+echo -e "% mainfile: ../master_thesis_GW.tex\n\\\chapter{${chapter_title}}" > "./TeX_files/${chapter_title}.tex"
+ln -s "../LaTeX/TeX_files/${chapter_title}.tex" "../${chapter_title}/${chapter_title}.tex"
+echo "\\input{\"./TeX_files/${chapter_title}.tex\"}"
+
+
---
+++ .bin/send_to_server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+scp ~/Studia_Warszawa/praca_magisterska_fizyka/LaTeX/master_thesis_GW.pdf ubuntu@sooyka.xyz:/media/ADATA/files
