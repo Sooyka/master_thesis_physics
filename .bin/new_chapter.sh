fd4512f6ff8c6037cc7c6028471a7c79014576ce
sooyka <bartosz.sjka@gmail.com>
2022-10-18 11:18:20 +0200

    new_chapter

--- .bin/new_chapter.sh
+++ .bin/new_chapter.sh
@@ -5,7 +5,10 @@ chapter_title=${1}
 
 mkdir "../${chapter_title}"
 touch "./TeX_files/${chapter_title}.tex"
-echo -e "% mainfile: ../master_thesis_GW.tex\n\\\chapter{${chapter_title}}" > "./TeX_files/${chapter_title}.tex"
+
+chapter_title_no_underscores=$(echo ${chapter_title} | sed 's/_/\ /')
+echo -e "% mainfile: ../master_thesis_GW.tex\n\\\chapter{${chapter_title_no_underscores}}" > "./TeX_files/${chapter_title}.tex"
+
 ln -s "../LaTeX/TeX_files/${chapter_title}.tex" "../${chapter_title}/${chapter_title}.tex"
 echo "\\input{\"./TeX_files/${chapter_title}.tex\"}"
 
